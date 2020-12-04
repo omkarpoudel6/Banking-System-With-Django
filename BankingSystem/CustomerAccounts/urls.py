31d8a434134df4c7f36c62871b31e02a58a8455e
omkarpoudel6 <omkar.poudel77@gmail.com>
2020-12-04 07:06:34 -0800

    Handaling Transaction Started with templates

---
+++ BankingSystem/CustomerAccounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('/',views.createAccount,name='create_new_account'),
+    path('/viewaccount',views.viewAccount,name='view_account'),
+    path('/withdraw',views.withdraw,name='withdraw_money'),
+    path('/deposit',views.deposit,name='deposie_money')
+]
--- BankingSystem/CustomerAccounts/views.py
+++ BankingSystem/CustomerAccounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def createAccount(request):
+    return render(request,'accountcreation.html')
+
+def viewAccount(reqeust):
+    return render(reqeust,'viewaccount.html')
+
+def deposit(request):
+    return render(request,'deposit.html')
+
+def withdraw(request):
+    return render(request,'withdraw.html')
