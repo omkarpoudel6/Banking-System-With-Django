376d247d45f118fe1de2dcfdffcdd70e56352301
omkarpoudel6 <omkar.poudel77@gmail.com>
2020-12-19 06:26:39 -0800

    Creating bank employee started

--- BankingSystem/BankingSystem/urls.py
+++ BankingSystem/BankingSystem/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from Users.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',login,name="login"),
     path('accounts/',include('CustomerAccounts.urls',namespace='accounts'))
 ]
