7f723a7dc604fbd06c35e57cc00f574d6eedc493
omkarpoudel6 <omkar.poudel77@gmail.com>
2020-12-26 07:18:00 -0800

    Updating User Profile Started

--- BankingSystem/Users/urls.py
+++ BankingSystem/Users/urls.py
@@ -6,5 +6,6 @@ app_name='manager'
 urlpatterns=[
     path("dashboard/",views.dashboard,name='dashboard'),
     path('create_user/',views.create_user,name='create_user'),
-    path('user_info/',views.user_info,name='user_info')
+    path('user_info/',views.user_info,name='user_info'),
+    path('change_user_password',views.change_user_password,name='change_user_password')
 ]
